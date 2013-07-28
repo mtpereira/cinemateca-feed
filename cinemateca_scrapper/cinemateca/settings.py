ef646db46020ac0fc0f1e0f1f2f768daf6559ce6
Francisco Covas <francisco.c.covas@gmail.com>
2013-07-28 17:04:36 +0100

    saving scrap results to mongodb

--- cinemateca_scrapper/cinemateca/settings.py
+++ cinemateca_scrapper/cinemateca/settings.py
@@ -11,9 +11,15 @@ BOT_NAME = 'cinemateca'
 SPIDER_MODULES = ['cinemateca.spiders']
 NEWSPIDER_MODULE = 'cinemateca.spiders'
 
+ITEM_PIPELINES = ['scrapy_mongodb.MongoDBPipeline']
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cinemateca (+http://www.yourdomain.com)'
 
 # ujson : exporter for utf8-encoded JSON
 FEED_EXPORTERS = {'ujson' : 'cinemateca.exporters.UnicodeJsonLinesItemExporter'}
 
+MONGODB_URI = 'mongodb://localhost:27017'
+MONGODB_DATABASE = 'scrapy'
+MONGODB_COLLECTION = 'movies'
+MONGODB_UNIQUE_KEY = 'date'
