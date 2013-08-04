97ae5cf5230dc304eec05c78d75a075ec7b9a5d7
Francisco Covas <francisco.c.covas@gmail.com>
2013-08-04 16:58:52 +0100

    added search by month

--- cinemateca-api/server.js
+++ cinemateca-api/server.js
@@ -9,37 +9,48 @@ server
 	.use(restify.bodyParser());
 
 server.get('/movies/:year/:month/:day', function(req, res, next){
-	var filename = req.params.year + '-' + req.params.month + '-' + req.params.day + '.json';
-	var filepath = process.env.HOME + '/events/' + filename;
 
-	new mongodb.Db('scrapy', new mongodb.Server('localhost', 27017, {auto_reconnect: true}), {'safe':false}).open(function(err, db){
-		if(err){
-			console.log(err);
-		}
+  var initDate = moment.utc(req.params.year + '-' + req.params.month + '-' + req.params.day);
 
-		db.collection('movies', function(error, collection){
-			if(err){
-				console.log(err);
-			}
+  var endDate = moment(initDate).add('days', 1);
 
-			var initDate = moment.utc(req.params.year + '-' + req.params.month + '-' + req.params.day);
-				
-			var endDate = moment(initDate).add('days', 1);
+  queryMovies(initDate, endDate, res);
 
-			collection.find(
-				{'date': { $lte: endDate.toDate(), $gte: initDate.toDate()}}
-				,{} 
-				//,{explain:1}
-			).toArray(function(err, items){
-				res.header('Content-Type', 'application/json; charset=utf-8')
-
-				console.log(items);
-
-				res.send(items);
-			})
-		})
-	});
 });
+
+server.get('/movies/:year/:month', function(req, res, next){
+
+  var initDate = moment.utc(req.params.year + '-' + req.params.month + '-01');
+
+  var endDate = moment(initDate).add('month', 1);
+
+  queryMovies(initDate, endDate, res);
+
+});
+
+function queryMovies(initDate, endDate, res){
+  new mongodb.Db('scrapy', new mongodb.Server('localhost', 27017, {auto_reconnect: true}), {'safe':false}).open(function(err, db){
+    if(err){
+      console.log(err);
+    }
+
+    db.collection('movies', function(error, collection){
+      if(err){
+        console.log(err);
+      }
+
+      collection.find(
+        {'date': { $lt: endDate.toDate(), $gte: initDate.toDate()}}
+        ,{}
+        //,{explain:1}
+      ).toArray(function(err, items){
+        res.header('Content-Type', 'application/json; charset=utf-8')
+
+        res.send(items);
+      })
+    })
+  });
+}
 
 server.listen(8080, function(){
 	console.log('%s listening at %s', server.name, server.url);
